15fed3e05a1dd0684f25c7b1cc3518e642ccd135
Michael Großhans <grosshan@cs.uni-potsdam.de>
2014-12-19 15:07:34 +0100

    all tests implemented

--- unittests/soundcloud/server/UserRegistryTest.java
+++ unittests/soundcloud/server/UserRegistryTest.java
@@ -7,8 +7,28 @@ import org.junit.Test;
 public class UserRegistryTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void registerTest() {
+		User u1 = new User(1);
+		User u2 = new User(2);
+		User u3 = new User(3);
+		UserRegistry registry = new UserRegistry();
+		registry.registerUser(1);
+		registry.registerUser(u2);
+		registry.registerUser(u3);
+		
+		assertNotNull(registry.getAllUser());
+		assertTrue(registry.getAllUser().size() == 3);
+		assertTrue(registry.getAllUser().contains(u1));
+		assertTrue(registry.getAllUser().contains(u2));
+		assertTrue(registry.getAllUser().contains(u3));
+
+		assertTrue(registry.getUser(1) == u1);
+		assertTrue(registry.getUser(2) == u2);
+		assertTrue(registry.getUser(3) == u3);
+	
+		assertTrue(registry.hasUser(1));
+		assertTrue(registry.hasUser(2));
+		assertTrue(registry.hasUser(3));
 	}
 
 }

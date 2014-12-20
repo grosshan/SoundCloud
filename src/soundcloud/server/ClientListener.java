ece8b67f272c8a01b637b6c7d12a0dee6d21681b
Michael Grosshans <grosshan@cs.uni-potsdam.de>
2014-12-20 15:51:13 +0100

    fixed warning in ClientListener

--- src/soundcloud/server/ClientListener.java
+++ src/soundcloud/server/ClientListener.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.List;
 
 public class ClientListener extends Thread {
 
